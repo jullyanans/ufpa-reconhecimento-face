9b792bd81f55ace3df4dbbe666002086cea7d274
jullyanans <nsjullyana@gmail.com>
2016-03-04 10:35:09 -0300

    Update urf_knn.m

--- urf_knn.m
+++ urf_knn.m
@@ -1,13 +1,13 @@
 function c = urf_knn(P, y, Q,  k)
-    n = size(P,2);    %retorna o tamanho da matriz da imagens de treino
-    if (nargin == 3)
+    n = size(P,2);      %retorna o tamanho da matriz da imagens de treino
+    if (nargin == 3)    %se os argumntos da função forem iguais a três, atribui 1 a k
         k=1;
-    elseif (k>n)
+    elseif (k>n)        %se forem diferentes de 3 e maior que o tamanho da matriz, atribui n a k
         k=n;
     end
 
-    Q = repmat(Q, 1, n);  %repete a matriz Q até ela ter n colunas
-    distancias = sqrt(sum(power((P-Q),2),1));  %% módulo da soma da diferença entre matriz de treino e matriz de teste
+    Q = repmat(Q, 1, n);     %repete a matriz Q até ela ter n colunas
+    distancias = sqrt(sum(power((P-Q),2),1));       % módulo da soma da diferença entre matriz de treino e matriz de teste
     [distancias, idx] = sort(distancias);
     y = y(idx);
     y = y(1:k);
